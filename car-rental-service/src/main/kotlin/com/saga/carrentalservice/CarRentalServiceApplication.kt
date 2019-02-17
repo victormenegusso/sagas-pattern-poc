479373354850d0a515d643473a6aa1b4cc2f6e96
Streppel <streppels@gmail.com>
2019-02-17 19:58:42 -0300

    creating car rental serice

---
+++ car-rental-service/src/main/kotlin/com/saga/carrentalservice/CarRentalServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.saga.carrentalservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarRentalServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarRentalServiceApplication>(*args)
+}
